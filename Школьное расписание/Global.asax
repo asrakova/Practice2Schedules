7c2987a9768d72eaf0006e098b23922fe74e7123
анна <@DESKTOP-14IJIEU>
2018-05-19 23:44:04 +0500

    Добавьте файлы проекта.

---
+++ Школьное расписание/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Школьное_расписание.MvcApplication" Language="C#" %>
---
+++ Школьное расписание/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Школьное_расписание.Startup))]
+namespace Школьное_расписание
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
